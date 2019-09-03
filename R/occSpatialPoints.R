e134505e064cad393b673fd480b71a96e76b6393
Eduardo Arlé <eduardo.arle@idiv.de>
2019-09-03 13:51:24 +0200

    Fix occSpatialPoints2

--- R/occSpatialPoints.R
+++ R/occSpatialPoints.R
@@ -2,15 +2,13 @@
 #'
 #' Downloads gbif records iterating when necessary to overcome the limitation of 200,000 records
 #'
-#' @import rgdal
-#' @import raster
 #' @import sp
 #' @param  occ table
 #' @return This function creates spatialpoints from tables containing coordinates.
 #' @export
 occSpatialPoints <- function(occ){
   occ_sp <- occ  #create spatial points from GBIF coordinates
-  coordinates(occ_sp) <- ~decimalLongitude+decimalLatitude
-  proj4string(occ_sp) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+  sp::coordinates(occ_sp) <- ~decimalLongitude+decimalLatitude
+  sp::proj4string(occ_sp) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
   return(occ_sp)
 }
