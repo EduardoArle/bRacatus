ade4d8c19e80d3965c90da616a00bc9c054f1692
Eduardo Arlé <eduardo.arle@idiv.de>
2020-06-11 13:20:00 +0200

    bugs

--- R/occID.R
+++ R/occID.R
@@ -13,7 +13,7 @@
 occID <- function(occ){
   occ_sp <- occSpatialPoints(occ)
   ID <- raster(res=1/2)
-  ID[] <- c(1:length(t))
+  ID[] <- c(1:length(ID))
   ID_points <- extract(ID,occ_sp)
   occ$ID_points <- ID_points
   return(occ)
